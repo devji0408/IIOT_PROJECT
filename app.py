7bc4839efab35367feef191790739fdd35a0afc0
devji0408 <panchdevmaddheshiya@gmail.com>
2026-05-01 13:08:02 +0530

    sync update

--- app.py
+++ app.py
@@ -315,7 +315,7 @@ elif menu == "🏆 Champions":
         ("2022","Jos Buttler"),
         ("2023","Shubman Gill"),
         ("2024","Virat Kohli"),
-        ("2025","Shubman Gill")
+        ("2025","Dev")
     ]
 
     for y,p in orange_cap:
@@ -343,7 +343,7 @@ elif menu == "🏆 Champions":
         ("2022","Yuzvendra Chahal"),
         ("2023","Mohammed Shami"),
         ("2024","Harshal Patel"),
-        ("2025","Jasprit Bumrah")
+        ("2025","Prasidh Krishna")
     ]
 
     for y,p in purple_cap:
